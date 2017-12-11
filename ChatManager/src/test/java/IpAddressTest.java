be1d25cdddcac8ccb6993f2fe56c13b4bd2c2aa0
damien <dnicolas@etud.insa-toulouse.fr>
2017-12-11 09:18:25 +0100

    ProcoleDeCommOk

---
+++ ChatManager/src/test/java/IpAddressTest.java
@@ -0,0 +1,31 @@
+import connectivite.AdresseIpSysteme;
+import main.ChatManager;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+public class IpAddressTest {
+
+
+    @Test
+    public void test() {
+        AdresseIpSysteme adIP = new AdresseIpSysteme();
+        ChatManager chatManager = new ChatManager();
+
+        try {
+            String adresseIPTest = adIP.ipInterfaceReseauToUser(chatManager);
+            System.out.println(adresseIPTest);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+
+
+}
+
